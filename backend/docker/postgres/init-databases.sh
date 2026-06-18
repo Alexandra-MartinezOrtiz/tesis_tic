49d41062e47bb48082601168b7242f7f57ffce56
Alexandra Martinez <maralexmar0@gmail.com>
2026-06-17 21:32:55 -0500

    Run all postgres init scripts

--- backend/docker/postgres/init-databases.sh
+++ backend/docker/postgres/init-databases.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-CREATE DATABASE ticfis_identity;
-CREATE DATABASE ticfis_propuestas;
-CREATE DATABASE ticfis_reportes;
+
+create_database() {
+  local database="$1"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+SELECT 'CREATE DATABASE ${database}'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${database}')\gexec
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d ticfis_identity -f /scripts/ticfis_identity/001_init.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d ticfis_propuestas -f /scripts/ticfis_propuestas/001_init.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d ticfis_reportes -f /scripts/ticfis_reportes/001_init.sql
+}
+
+run_scripts() {
+  local database="$1"
+  for script in /scripts/"$database"/*.sql; do
+    echo "Running $script on $database"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" -f "$script"
+  done
+}
+
+create_database ticfis_identity
+create_database ticfis_propuestas
+create_database ticfis_reportes
+
+run_scripts ticfis_identity
+run_scripts ticfis_propuestas
+run_scripts ticfis_reportes
